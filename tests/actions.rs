8638a607e5798a133a70bca165da3adba3d744c1
Rishabh Das <rdas@tutanota.com>
2023-01-15 16:09:58 +0530

    tests: fix add_child

--- tests/actions.rs
+++ tests/actions.rs
@@ -6,10 +6,10 @@ use grus::node::NodeData;
 #[test]
 fn add_child() -> Result<(), Error> {
 	test_action("add_child", |mut app| {
-		"one".chars().for_each(|c| app.status_view.pushc(c));
+		"one".chars().for_each(|c| app.status_view.command.push(c));
 		app.add_child()?;
 		app.tree_view.move_sel_down();
-		"two".chars().for_each(|c| app.status_view.pushc(c));
+		"two".chars().for_each(|c| app.status_view.command.push(c));
 		app.add_child()?;
 
 		let reader = app.store.reader()?;
