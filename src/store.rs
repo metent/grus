68022e659750e997999451ad650e667896bd8950
Rishabh Das <rdas@tutanota.com>
2023-01-20 10:32:49 +0530

    store: include doubly linked lists of siblings in store

--- src/store.rs
+++ src/store.rs
@@ -1,10 +1,10 @@
 use std::io::{Error as IoError, ErrorKind};
 use std::path::Path;
-use sanakirja::{Commit, Env, Error, MutTxn, RootDb, Storable, Txn};
+use sanakirja::{Commit, Env, Error, LoadPage, MutTxn, RootDb, Storable, Txn};
 use sanakirja::btree::{self, Db, UDb};
 
 type LinksDb = Db<u64, u64>;
-type RLinksDb = Db<u64, RPair>;
+type RLinksDb = Db<u64, RTriple>;
 type NodesDb = UDb<u64, [u8]>;
 
 const ID_SQ: usize = 0;
@@ -29,9 +29,11 @@ impl Store {
 
 	pub fn reader(&self) -> Result<StoreReader, Error> {
 		let txn = Env::txn_begin(&self.env)?;
+		let id = txn.root(ID_SQ);
 		let links = txn.root_db(DB_LINKS).ok_or_else(invalid_data_error)?;
+		let rlinks = txn.root_db(DB_RLINKS).ok_or_else(invalid_data_error)?;
 		let nodes = txn.root_db(DB_NODES).ok_or_else(invalid_data_error)?;
-		Ok(StoreReader { txn, links, nodes })
+		Ok(StoreReader { txn, id, links, rlinks, nodes })
 	}
 
 	pub fn writer(&self) -> Result<StoreWriter, Error> {
@@ -77,80 +79,110 @@ impl Store {
 	}
 }
 
-pub struct StoreReader<'env> {
-	txn: Txn<&'env Env>,
-	links: LinksDb,
-	nodes: NodesDb,
-}
+pub type StoreReader<'env> = StoreRw<Txn<&'env Env>>;
 
-impl<'env> StoreReader<'env> {
-	pub fn read(&self, id: u64) -> Result<Option<&[u8]>, Error> {
-		if let Some((&eid, data)) = btree::get(&self.txn, &self.nodes, &id, None)? {
-			if eid == id { Ok(Some(data)) } else { Ok(None) }
-		} else {
-			Ok(None)
-		}
-	}
-
-	pub fn children(&self, id: u64) -> Result<ChildIter, Error> {
-		Ok(ChildIter {
-			reader: self,
-			child_ids: btree::iter(&self.txn, &self.links, Some((&id, None)))?,
-			pid: id,
-		})
-	}
-}
-
-pub struct ChildIter<'env, 'reader> {
-	reader: &'reader StoreReader<'env>,
-	child_ids: btree::Iter<'reader, Txn<&'env Env>, u64, u64, btree::page::Page<u64, u64>>,
-	pid: u64,
-}
-
-impl<'env, 'reader> Iterator for ChildIter<'env, 'reader> {
-	type Item = Result<(u64, &'reader [u8]), Error>;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		let child_id = match self.child_ids.next()? {
-			Ok((&pid, child_id)) => {
-				if pid != self.pid { return None }
-				child_id
-			}
-			Err(err) => return Some(Err(err)),
-		};
-		match btree::get(&self.reader.txn, &self.reader.nodes, child_id, None) {
-			Ok(Some((&id, data))) => Some(Ok((id, data))),
-			Ok(None) => Some(Err(invalid_data_error())),
-			Err(err) => Some(Err(err)),
-		}
-	}
-}
-
-pub struct StoreWriter<'env> {
-	txn: MutTxn<&'env Env, ()>,
+pub struct StoreRw<T: LoadPage> {
+	txn: T,
 	id: u64,
 	links: LinksDb,
 	rlinks: RLinksDb,
 	nodes: NodesDb,
 }
 
+impl<T: LoadPage<Error = Error>> StoreRw<T> {
+	pub fn read(&self, id: u64) -> Result<Option<&[u8]>, Error> {
+		match btree::get(&self.txn, &self.nodes, &id, None)? {
+			Some((&eid, data)) if eid == id => Ok(Some(data)),
+			_ => Ok(None)
+		}
+	}
+
+	pub fn children<'s>(&'s self, id: u64) -> Result<impl Iterator<Item = Result<(u64, &'s[u8]), Error>>, Error> {
+		Ok(self.child_ids(id)?.map(|child_id| match btree::get(&self.txn, &self.nodes, &child_id?, None)? {
+			Some((&child_id, data)) => Ok((child_id, data)),
+			None => Err(invalid_data_error()),
+		}))
+	}
+
+	fn child_ids(&self, id: u64) -> Result<ChildIdIter<'_, T>, Error> {
+		Ok(ChildIdIter {
+			reader: self,
+			child_ids: ChildIds::new(self, id)?,
+		})
+	}
+
+	fn get_child(&self, id: u64) -> Result<Option<u64>, Error> {
+		match btree::get(&self.txn, &self.links, &id, None)? {
+			Some((&eid, &child)) if eid == id => Ok(Some(child)),
+			_ => Ok(None)
+		}
+	}
+
+	fn get_rt(&self, id: u64, pid: u64) -> Result<Option<RTriple>, Error> {
+		match btree::get(&self.txn, &self.rlinks, &id, Some(&RTriple { pid, next: 0, prev: 0 }))? {
+			Some((&eid, &rt)) if eid == id && rt.pid == pid => Ok(Some(rt)),
+			_ => Ok(None)
+		}
+	}
+}
+
+pub struct ChildIdIter<'reader, T: LoadPage<Error = Error>> {
+	reader: &'reader StoreRw<T>,
+	child_ids: ChildIds,
+}
+
+impl<'reader, T: LoadPage<Error = Error>> Iterator for ChildIdIter<'reader, T> {
+	type Item = Result<u64, Error>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.child_ids.next(self.reader).transpose()
+	}
+}
+
+struct ChildIds {
+	pid: u64,
+	id: u64,
+}
+
+impl ChildIds {
+	fn new<T: LoadPage<Error = Error>>(reader: &StoreRw<T>, id: u64) -> Result<Self, Error> {
+		Ok(ChildIds { pid: id, id: reader.get_child(id)?.unwrap_or(0) })
+	}
+
+	fn next<T: LoadPage<Error = Error>>(&mut self, reader: &StoreRw<T>) -> Result<Option<u64>, Error> {
+		let id = self.id;
+		if id == 0 { return Ok(None) };
+		let RTriple { next, .. } = reader.get_rt(id, self.pid)?.ok_or_else(invalid_data_error)?;
+		self.id = next;
+		Ok(Some(id))
+	}
+}
+
+type StoreWriter<'env> = StoreRw<MutTxn<&'env Env, ()>>;
+
 impl<'env> StoreWriter<'env> {
 	pub fn add_child(&mut self, pid: u64, data: &[u8]) -> Result<u64, Error> {
+		let next = self.get_child(pid)?.unwrap_or(0);
+
 		let id = self.id;
+		btree::del(&mut self.txn, &mut self.links, &pid, None)?;
 		btree::put(&mut self.txn, &mut self.links, &pid, &id)?;
-		btree::put(&mut self.txn, &mut self.rlinks, &id, &RPair { pid, prio: id })?;
+
+		btree::put(&mut self.txn, &mut self.rlinks, &id, &RTriple { pid, next, prev: 0 })?;
+		if next > 0 { self.modify_rt(next, pid, |rt| rt.prev = id)? };
+
 		btree::put(&mut self.txn, &mut self.nodes, &id, data)?;
 		self.id += 1;
 		Ok(id)
 	}
 
 	pub fn delete(&mut self, pid: u64, id: u64) -> Result<(), Error> {
-		if !btree::del(&mut self.txn, &mut self.links, &pid, Some(&id))? { return Ok(()) };
+		btree::del(&mut self.txn, &mut self.links, &pid, Some(&id))?;
 
-		let (&eid, &rpair) = btree::get(&self.txn, &self.rlinks, &id, Some(&RPair { pid, prio: 0 }))?
-			.ok_or_else(invalid_data_error)?;
-		if eid != id || pid != rpair.pid { return Err(invalid_data_error()) };
-		btree::del(&mut self.txn, &mut self.rlinks, &id, Some(&rpair))?;
+		let rt = self.get_rt(id, pid)?.ok_or_else(invalid_data_error)?;
+		if rt.prev > 0 { self.modify_rt(rt.prev, pid, |prt| prt.next = rt.next)? };
+		if rt.next > 0 { self.modify_rt(rt.next, pid, |nrt| nrt.prev = rt.prev)? };
+		btree::del(&mut self.txn, &mut self.rlinks, &id, Some(&rt))?;
 
 		if let Some((&eid, _)) = btree::get(&self.txn, &self.rlinks, &id, None)? {
 			if eid == id { return Ok(()) };
@@ -158,12 +190,9 @@ impl<'env> StoreWriter<'env> {
 
 		btree::del(&mut self.txn, &mut self.nodes, &id, None)?;
 
-		while let Some((&eid, &child_id)) = btree::get(&self.txn, &self.links, &id, None)? {
-			if eid == id {
-				self.delete(id, child_id)?;
-			} else {
-				break;
-			}
+		let mut child_ids = ChildIds::new(self, id)?;
+		while let Some(child_id) = child_ids.next(self)? {
+			self.delete(id, child_id)?;
 		}
 		Ok(())
 	}
@@ -176,13 +205,15 @@ impl<'env> StoreWriter<'env> {
 
 	pub fn share(&mut self, src: u64, dest: u64) -> Result<bool, Error> {
 		if self.is_descendent_of(dest, src)? { return Ok(false) };
-		if let Some((&pid, &id)) = btree::get(&self.txn, &self.links, &dest, Some(&src))? {
-			if pid == dest && id == src { return Ok(false) };
-		}
+		if self.get_rt(src, dest)?.is_some() { return Ok(false) };
 
+		let next = self.get_child(dest)?.unwrap_or(0);
+
+		btree::del(&mut self.txn, &mut self.links, &dest, None)?;
 		btree::put(&mut self.txn, &mut self.links, &dest, &src)?;
-		btree::put(&mut self.txn, &mut self.rlinks, &src, &RPair { pid: dest, prio: self.id })?;
-		self.id += 1;
+
+		btree::put(&mut self.txn, &mut self.rlinks, &src, &RTriple { pid: dest, next, prev: 0 })?;
+		if next > 0 { self.modify_rt(next, dest, |rt| rt.prev = src)? };
 
 		Ok(true)
 	}
@@ -192,10 +223,10 @@ impl<'env> StoreWriter<'env> {
 
 		btree::del(&mut self.txn, &mut self.links, &src_pid, Some(&src))?;
 
-		let (&eid, &rpair) = btree::get(&self.txn, &self.rlinks, &src, Some(&RPair { pid: src_pid, prio: 0 }))?
-			.ok_or_else(invalid_data_error)?;
-		if eid != src || src_pid != rpair.pid { return Err(invalid_data_error()) };
-		btree::del(&mut self.txn, &mut self.rlinks, &src, Some(&rpair))?;
+		let rt = self.get_rt(src, src_pid)?.ok_or_else(invalid_data_error)?;
+		if rt.prev > 0 { self.modify_rt(rt.prev, src_pid, |prt| prt.next = rt.next)? };
+		if rt.next > 0 { self.modify_rt(rt.next, src_pid, |nrt| nrt.prev = rt.prev)? };
+		btree::del(&mut self.txn, &mut self.rlinks, &src, Some(&rt))?;
 
 		Ok(true)
 	}
@@ -208,24 +239,31 @@ impl<'env> StoreWriter<'env> {
 		self.txn.commit()
 	}
 
+	fn modify_rt(&mut self, id: u64, pid: u64, f: impl Fn(&mut RTriple)) -> Result<(), Error> {
+		let mut rt = self.get_rt(id, pid)?.ok_or_else(invalid_data_error)?;
+		btree::del(&mut self.txn, &mut self.rlinks, &id, Some(&rt))?;
+		f(&mut rt);
+		btree::put(&mut self.txn, &mut self.rlinks, &id, &rt)?;
+		Ok(())
+	}
+
 	fn is_descendent_of(&self, subj: u64, pred: u64) -> Result<bool, Error> {
 		if subj == pred { return Ok(true) };
-		for entry in btree::iter(&self.txn, &self.links, Some((&pred, None)))? {
-			let (&id, &child_id) = entry?;
-			if id != pred { break };
-			if self.is_descendent_of(subj, child_id)? { return Ok(true) };
+		for child_id in self.child_ids(pred)? {
+			if self.is_descendent_of(subj, child_id?)? { return Ok(true) };
 		}
 		Ok(false)
 	}
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
-struct RPair {
+struct RTriple {
 	pid: u64,
-	prio: u64,
+	next: u64,
+	prev: u64,
 }
 
-impl Storable for RPair {
+impl Storable for RTriple {
 	type PageReferences = core::iter::Empty<u64>;
 	fn page_references(&self) -> Self::PageReferences {
 		core::iter::empty()
