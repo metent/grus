e0fd2a495700f720f983b204f05d210a50c6f3d6
Rishabh Das <rdas@tutanota.com>
2023-07-15 12:19:27 +0530

    parser: update to winnow v0.5.0

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,13 @@
 use chrono::{Datelike, Local, NaiveDate, NaiveDateTime, NaiveTime, Weekday};
 use chrono::naive::Days;
 use grus_lib::types::Session;
-use winnow::{IResult, Parser};
-use winnow::branch::alt;
-use winnow::bytes::{tag, tag_no_case, take, take_until1, take_while0, take_while1};
-use winnow::error::{ErrMode, Error, ErrorKind};
-use winnow::sequence::separated_pair;
+use winnow::{Parser, PResult};
+use winnow::combinator::{alt, separated_pair};
+use winnow::token::{tag, tag_no_case, take, take_until1, take_while};
+use winnow::error::{ContextError, ErrMode, ParseError};
 use winnow::stream::AsChar;
 
-pub fn parse_session(s: &str) -> Result<Session, Error<&str>> {
+pub fn parse_session(s: &str) -> Result<Session, ParseError<&str, ContextError>> {
 	alt((
 		separated_pair(datetime, tag(" to "), datetime)
 			.map(|(dt1, dt2)| Session { start: dt1, end: dt2 }),
@@ -25,7 +24,7 @@ pub fn parse_session(s: &str) -> Result<Session, Error<&str>> {
 	)).parse(s)
 }
 
-pub fn parse_datetime(s: &str) -> Result<NaiveDateTime, Error<&str>> {
+pub fn parse_datetime(s: &str) -> Result<NaiveDateTime, ParseError<&str, ContextError>> {
 	alt((
 		datetime,
 		time.map(|time| NaiveDateTime::new(Local::now().date_naive(), time)),
@@ -33,52 +32,52 @@ pub fn parse_datetime(s: &str) -> Result<NaiveDateTime, Error<&str>> {
 	)).parse(s)
 }
 
-fn datetime(s: &str) -> IResult<&str, NaiveDateTime> {
-	let (s, date) = date(s)?;
-	let (s, _) = take_while0(AsChar::is_space).parse_next(s)?;
-	let (s, time) = time(s)?;
-	Ok((s, NaiveDateTime::new(date, time)))
+fn datetime(s: &mut &str) -> PResult<NaiveDateTime> {
+	let date = date(s)?;
+	take_while(0.., AsChar::is_space).parse_next(s)?;
+	let time = time(s)?;
+	Ok(NaiveDateTime::new(date, time))
 }
 
-fn time(s: &str) -> IResult<&str, NaiveTime> {
+fn time(s: &mut &str) -> PResult<NaiveTime> {
 	alt((
 		proper_time,
 		quick_time,
 	)).parse_next(s)
 }
 
-fn proper_time(s: &str) -> IResult<&str, NaiveTime> {
-	let (s, hour) = take_until1(":").parse_next(s)?;
-	let (s, _) = tag(":").parse_next(s)?;
-	let (s, minute) = take(2usize).parse_next(s)?;
-	let (s, _) = take_while0(AsChar::is_space).parse_next(s)?;
-	let (s, delta) = alt((
+fn proper_time(s: &mut &str) -> PResult<NaiveTime> {
+	let hour = take_until1(":").parse_next(s)?;
+	tag(":").parse_next(s)?;
+	let minute = take(2usize).parse_next(s)?;
+	take_while(0.., AsChar::is_space).parse_next(s)?;
+	let delta = alt((
 		alt((tag("am"), tag("AM"))).map(|_| 0),
 		alt((tag("pm"), tag("PM"))).map(|_| 12),
 	)).parse_next(s)?;
-	let mut h = hour.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?;
-	if h > 12 { return Err(ErrMode::Cut(Error::new(s, ErrorKind::Token))) }
+	let mut h = hour.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?;
+	if h > 12 { return Err(ErrMode::Cut(ContextError::new())) }
 	if h == 12 { h = 0 }
 	h += delta;
-	let m = minute.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?;
-	Ok((s, NaiveTime::from_hms_opt(h, m, 0).ok_or(ErrMode::Cut(Error::new(s, ErrorKind::Token)))?))
+	let m = minute.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?;
+	Ok(NaiveTime::from_hms_opt(h, m, 0).ok_or(ErrMode::Cut(ContextError::new()))?)
 }
 
-fn quick_time(s: &str) -> IResult<&str, NaiveTime> {
-	let (s, hour) = take_while1(AsChar::is_dec_digit).parse_next(s)?;
-	let (s, _) = take_while0(AsChar::is_space).parse_next(s)?;
-	let (s, delta) = alt((
+fn quick_time(s: &mut &str) -> PResult<NaiveTime> {
+	let hour = take_while(1.., AsChar::is_dec_digit).parse_next(s)?;
+	take_while(0.., AsChar::is_space).parse_next(s)?;
+	let delta = alt((
 		alt((tag("am"), tag("AM"))).map(|_| 0),
 		alt((tag("pm"), tag("PM"))).map(|_| 12),
 	)).parse_next(s)?;
-	let mut h = hour.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?;
-	if h > 12 { return Err(ErrMode::Cut(Error::new(s, ErrorKind::Token))) }
+	let mut h = hour.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?;
+	if h > 12 { return Err(ErrMode::Cut(ContextError::new())) }
 	if h == 12 { h = 0 }
 	h += delta;
-	Ok((s, NaiveTime::from_hms_opt(h, 0, 0).ok_or(ErrMode::Cut(Error::new(s, ErrorKind::Token)))?))
+	Ok(NaiveTime::from_hms_opt(h, 0, 0).ok_or(ErrMode::Cut(ContextError::new()))?)
 }
 
-fn date(s: &str) -> IResult<&str, NaiveDate> {
+fn date(s: &mut &str) -> PResult<NaiveDate> {
 	alt((
 		tag_no_case("today").map(|_| Local::now().date_naive()),
 		tag_no_case("yesterday").map(|_| Local::now().date_naive() - Days::new(1)),
@@ -89,8 +88,8 @@ fn date(s: &str) -> IResult<&str, NaiveDate> {
 	)).parse_next(s)
 }
 
-fn weekday(s: &str) -> IResult<&str, NaiveDate> {
-	let (s, weekday) = alt((
+fn weekday(s: &mut &str) -> PResult<NaiveDate> {
+	let weekday = alt((
 		alt((tag_no_case("monday"), tag_no_case("mon"))).map(|_| Weekday::Mon),
 		alt((tag_no_case("tuesday"), tag_no_case("tue"))).map(|_| Weekday::Tue),
 		alt((tag_no_case("wednesday"), tag_no_case("wed"))).map(|_| Weekday::Wed),
@@ -101,18 +100,18 @@ fn weekday(s: &str) -> IResult<&str, NaiveDate> {
 	)).parse_next(s)?;
 	let today = Local::now().date_naive();
 	let delta = (weekday.num_days_from_monday() + 7 - today.weekday().num_days_from_monday()) % 7;
-	Ok((s, today + Days::new(delta.into())))
+	Ok(today + Days::new(delta.into()))
 }
 
-fn ddmmyyyy(s: &str) -> IResult<&str, NaiveDate> {
-	let (s, day) = take(2usize).parse_next(s)?;
-	let (s, _) = tag("/").parse_next(s)?;
-	let (s, month) = take(2usize).parse_next(s)?;
-	let (s, _) = tag("/").parse_next(s)?;
-	let (s, year) = take(4usize).parse_next(s)?;
-	Ok((s, NaiveDate::from_ymd_opt(
-		year.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?,
-		month.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?,
-		day.parse().map_err(|_| ErrMode::Cut(Error::new(s, ErrorKind::Token)))?,
-	).ok_or(ErrMode::Cut(Error::new(s, ErrorKind::Token)))?))
+fn ddmmyyyy(s: &mut &str) -> PResult<NaiveDate> {
+	let day = take(2usize).parse_next(s)?;
+	tag("/").parse_next(s)?;
+	let month = take(2usize).parse_next(s)?;
+	tag("/").parse_next(s)?;
+	let year = take(4usize).parse_next(s)?;
+	Ok(NaiveDate::from_ymd_opt(
+		year.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?,
+		month.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?,
+		day.parse().map_err(|_| ErrMode::Cut(ContextError::new()))?,
+	).ok_or(ErrMode::Cut(ContextError::new()))?)
 }
